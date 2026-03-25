b9277cfc1bc7eb29b4ecbd4bdad96e17ff1cd5a6
Muhammad Maulana Agung <muhammadmaulanaagung67@gmail.com>
2026-03-26 01:44:47 +0700

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,5 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
 });
-console.log("URL API yang dipakai:", import.meta.env.VITE_BASE_URL);
 
 export default api;
